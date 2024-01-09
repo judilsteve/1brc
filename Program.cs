22a53cbb40ed8064735274ce745e2b081bb35c86
james <judilsteve>
2024-01-09 17:36:01 +0800

    38s

--- Program.cs
+++ Program.cs
@@ -2,8 +2,6 @@ using System.Globalization;
 using System.Collections.Concurrent;
 using System.IO.MemoryMappedFiles;
 
-var cityStatMap = new ConcurrentDictionary<string, CityStats>(-1, 10000);
-
 using var memoryMappedMeasurements = MemoryMappedFile.CreateFromFile("measurements.txt");
 var chunkRanges = new List<(long, long)>();
 long nextChunkStart = 0;
@@ -16,12 +14,11 @@ using (var accessor = memoryMappedMeasurements.CreateViewStream()) {
         nextChunkStart = accessor.Position;
     }
     chunkRanges.Add((nextChunkStart, accessor.Length - 1));
-
-    Console.WriteLine($"Length: {accessor.Length}");
-    foreach(var (startIndex, endIndex) in chunkRanges) Console.WriteLine($"Chunk: {startIndex} -> {endIndex}");
 }
 
+var chunkResults = new ConcurrentBag<Dictionary<string, CityStats>>();
 Parallel.ForEach(chunkRanges, range => {
+    var cities = new Dictionary<string, CityStats>(10000);
     var (startIndex, endIndex) = range;
     using var accessor = memoryMappedMeasurements.CreateViewStream(startIndex, endIndex - startIndex);
     using var reader = new StreamReader(accessor);
@@ -29,14 +26,30 @@ Parallel.ForEach(chunkRanges, range => {
     while((line = reader.ReadLine()) is not null) {
         var separatorIndex = line.IndexOf(';');
         var city = line[..separatorIndex];
-        var temp = double.Parse(line[(separatorIndex + 1)..], CultureInfo.InvariantCulture);
-        var cityStats = cityStatMap.GetOrAdd(city, new CityStats());
-        cityStats.AddMeasurement(temp);
+        var temp = double.Parse(line.AsSpan(separatorIndex + 1), CultureInfo.InvariantCulture);
+        if (!cities.TryGetValue(city, out var cityStats)) {
+            cityStats = new CityStats(temp);
+            cities[city] = cityStats;
+        } else {
+            cityStats.AddMeasurement(temp);
+        }
     }
+    chunkResults.Add(cities);
 });
 
-foreach (var kvp in cityStatMap.OrderBy(kvp => kvp.Key)) {
-    Console.WriteLine($"{kvp.Key}={kvp.Value}");
+var combined = new Dictionary<string, CityStats>(10000);
+foreach(var result in chunkResults) {
+    foreach(var (city, cityStatsChunk) in result) {
+        if(combined.TryGetValue(city, out var cityStatsCombined)) {
+            combined[city] = new CityStats(combined[city], cityStatsChunk);
+        } else {
+            combined[city] = cityStatsChunk;
+        }
+    }
+}
+
+foreach (var (city, stats) in combined.OrderBy(kvp => kvp.Key)) {
+    Console.WriteLine($"{city}={stats}");
 }
 
 class CityStats
@@ -44,18 +57,28 @@ class CityStats
     private double min;
     private double max;
     private double sum;
-    private int count = 1;
+    private int count;
+
+    public CityStats(double firstValue) {
+        min = firstValue;
+        max = firstValue;
+        sum = firstValue;
+        count = 1;
+    }
+
+    public CityStats(CityStats a, CityStats b) {
+        min = Math.Min(a.min, b.min);
+        max = Math.Max(a.max, b.max);
+        sum = a.sum + b.sum;
+        count = a.count + b.count;
+    }
 
     public void AddMeasurement(double value)  {
-        lock(this) {
-            if (value < min) min = value;
-            else if (value > max) max = value;
-            sum += value;
-            count++;
-        }
+        if (value < min) min = value;
+        else if (value > max) max = value;
+        sum += value;
+        count++;
     }
 
-    public override string ToString() {
-        return $"{min:F1}/{sum / count:F1}/{max:F1}";
-    }
+    public override string ToString() => $"{min:F1}/{sum / count:F1}/{max:F1}";
 }
